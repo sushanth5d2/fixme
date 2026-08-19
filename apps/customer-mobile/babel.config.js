b36ebe644303517f46c4a639125fbcfc35c286bb
sushanth Chithaluri <19j41a05d2@gmail.com>
2026-08-19 18:04:26 +0530

    syntax error is fixed

--- apps/customer-mobile/babel.config.js
+++ apps/customer-mobile/babel.config.js
@@ -1,7 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          jsxRuntime: 'automatic',
+        },
+      ],
+    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
--- apps/fixer-mobile/metro.config.js
+++ apps/fixer-mobile/metro.config.js
@@ -15,4 +15,7 @@ config.resolver.nodeModulesPaths = [
   path.resolve(monorepoRoot, 'node_modules'),
 ];
 
+// 3. Prevent duplicate/nested copies of react-native inside node_modules/expo
+config.resolver.disableHierarchicalLookup = true;
+
 module.exports = config;
